74c555404aa879d3bba695dece4846d863b7ce0f
Pierre-Luc M. <pierre-luc.millet@student.junia.com>
2024-05-22 19:46:29 +0200

    scriptable object for procedural generation
    set multiple parameters for generate a room

---
+++ Assets/Scripts/Scriptable Objects Scripts/Pro-Gen/ProGenParams.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/ProGenParams", order = 3)]
+public class ProGenParams:ScriptableObject
+{
+    [Header("Room Generation Parameters")]
+    [Tooltip("Width of the grid")]
+    public int width;
+    [Tooltip("Height of the grid")]
+    public int height;
+    
+    [Tooltip("Width offset is the length of one side of the wall prefab e.g 2.5f")]
+    public float widthOffset=2.5f;
+    [Tooltip("Height offset is the length of one side of the wall prefab e.g 2.5f")]
+    public float heightOffset=2.5f;
+    
+    [Header("Room Generation Seeds")]
+    public int roomSeed;
+    
+    [Header("Prefabs")]
+    public RoomCell RoomCellPrefab;
+
+    public List<GameObject> WallDoorPrefabs;
+    public List<GameObject> WallWindowsPrefabs;
+
+    [Header("Custom Room Generation Parameters")]
+    public int WindowNumberRatio;
+    public int DoorNumberRatio;
+    public int ObjectNumberRatio;
+    public int WindowPerWallNumber;
+    public int DoorPerWallNumber;
+}
