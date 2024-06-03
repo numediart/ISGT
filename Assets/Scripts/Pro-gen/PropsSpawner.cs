2ab6ee897dba428733648eb4bad0f5708da7cb3c
Tom Ruisseau <tom.ruisseau.300302@gmail.com>
2024-06-03 15:51:16 +0200

    Chairs in front of tables

--- Assets/Scripts/Pro-gen/PropsSpawner.cs
+++ Assets/Scripts/Pro-gen/PropsSpawner.cs
@@ -19,6 +19,7 @@ namespace Pro_gen
                     SpawnTV();
                     break;
                 case TypeOfProp.Chair:
+                    SpawnChairs();
                     break;
                 case TypeOfProp.Armchair:
                     break;
@@ -28,9 +29,6 @@ namespace Pro_gen
         private void SpawnTV()
         {
             int randomIndex = _random.Next(_SpawnablePrefabs.Count + 1);
-            
-            Debug.Log("Random index: " + randomIndex);
-            
             
             if (randomIndex == _SpawnablePrefabs.Count)
             {
@@ -42,14 +40,50 @@ namespace Pro_gen
             //Make the TV go up by half of its height
             TV.transform.position += new Vector3(0, TV.transform.localScale.y / 2, 0);
             
-            //Make TV rotation equal to the forward of this
+            //Make TV rotation equal to the forward of this, and add a random rotation between -6 and 6 degrees
             TV.transform.rotation = transform.rotation;
+            
+            
+            float randomRotation = _random.Next(-60, 60);
+            TV.transform.Rotate(0, randomRotation / 10, 0);
             
             //Make the TV a child of the spawn point
             TV.transform.parent = _SpawnPoints[0].transform;
         }
         
+        private void SpawnChairs()
+        {
+            foreach (var spawnPoint in _SpawnPoints)
+            {
+                int randomIndex = _random.Next(_SpawnablePrefabs.Count + 1);
+                
+                if (randomIndex != _SpawnablePrefabs.Count)
+                {
+                    GameObject chair = Instantiate(_SpawnablePrefabs[randomIndex], spawnPoint.transform.position,
+                        Quaternion.identity);
         
+                    chair.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+
+                    if (_SpawnPoints.Count > 4) //the table is a rectangle
+                    {
+                        //make the table rotation match the forward of the spawn point
+                        chair.transform.rotation = spawnPoint.transform.rotation;
+                    }
+                    else //the table is round
+                    {
+                        // Make the chair look at the table horizontally
+                        Vector3 lookAtPosition = new Vector3(transform.position.x, chair.transform.position.y, transform.position.z);
+                        chair.transform.LookAt(lookAtPosition);
+                    }
+                
+
+                    // Make the chair a child of the spawn point
+                    chair.transform.parent = spawnPoint.transform;
+                }
+                
+            }
+        }
+
     }
 
     enum TypeOfProp
